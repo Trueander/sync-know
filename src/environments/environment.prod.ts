2cb05c048bbc54adf2c2ae77b822be7122ce1795
Ander <anderson.bengolea@scanntech.com>
2024-10-13 09:34:53 -0500

    changing API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gestion-conocimiento-back-259998362001.us-central1.run.app/api'
+  apiUrl: 'https://gestion-conocimiento-259998362001.us-central1.run.app/api'
 }
